c790328915712fb6d14483fa8e290214281f0dc4
0xA79D2E <167731823+0xA79D2E@users.noreply.github.com>
2025-04-11 13:11:35 -0700

    Update pan-tilt-laser.ino

--- pan-tilt-laser.ino
+++ pan-tilt-laser.ino
@@ -18,8 +18,7 @@ void setup() {
   Serial.begin(9600);
 
   servoX.attach(D3, PULSE_LOW, X_PULSE_HIGH);
-  servoY.attach(D5, PULSE_LOW, Y_
-PULSE_HIGH);
+  servoY.attach(D5, PULSE_LOW, Y_PULSE_HIGH);
 
 }
 
@@ -32,13 +31,13 @@ void loop() {
     int xIndex = input.indexOf('X');
     int yIndex = input.indexOf('Y');
     if (xIndex >= 0 && yIndex > xIndex) {
-      pan = input.substring(xIndex + 1, yIndex).toInt();
-      tilt = input.substring(yIndex + 1).toInt();
-      microX = map(pan, X_ANGLE_LOW, X_ANGLE_HIGH, PULSE_LOW, X_PULSE_HIGH);
-      microY = map(tilt, Y_ANGLE_LOW, Y_ANGLE_HIGH, PULSE_LOW< Y_PULSE_HIGH);
-      servoX.writeMicroseconds(microX);
-      servoY.writeMicroseconds(microY);
-      delay(50);
+pan = input.substring(xIndex + 1, yIndex).toInt();
+tilt = input.substring(yIndex + 1).toInt();
+
+servoX.writeMicroseconds(pan);
+servoY.writeMicroseconds(tilt);
+
+      // delay(5);
     } 
   }
 }
