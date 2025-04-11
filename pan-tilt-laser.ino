8e23fbf5f385120e791ad72ab48e330e9a7d4d23
0xA79D2E <167731823+0xA79D2E@users.noreply.github.com>
2025-04-11 12:36:24 -0700

    Update pan-tilt-laser.ino

--- pan-tilt-laser.ino
+++ pan-tilt-laser.ino
@@ -1,4 +1,16 @@
+#include <Servo.h>
+#define PULSE_LOW 387
+#define PULSE_HIGH_X 2400
+#define PULSE_HIGH_Y 2300
+
+Servo servoX;
+Servo servoY;
+
+
 void setup() {
+  Serial.begin(9600);
+  servoX.attach(D3, PULSE_LOW, PULSE_HIGH_X);
+  servoY.attach(D5, PULSE_LOW, PULSE_HIGH_Y);
 
 }
 
